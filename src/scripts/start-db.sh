c96da2448504e0b1a78727291a4438128c323f62
joe.worsfold <joseph.worsfold@infinityworks.com>
2021-12-06 14:02:47 +0000

    config via .env

---
+++ src/scripts/start-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -o allexport; source ../../.env; set +o allexport
+
+echo "echo stop & remove old docker [$POSTGRES_SERVER] and starting new fresh instance of [$POSTGRES_SERVER]"
+(docker kill $POSTGRES_SERVER || :) && \
+  (docker rm $POSTGRES_SERVER || :) && \
+  docker run --name $POSTGRES_SERVER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e PGPASSWORD=$POSTGRES_PASSWORD \
+  -p 5432:5432 \
+  -d postgres
+
+# wait for pg to start
+echo "sleep wait for pg-server [$POSTGRES_SERVER] to start";
+SLEEP 3;
+
+# create the db
+echo "CREATE DATABASE $POSTGRES_DATABASE ENCODING 'UTF-8';" | docker exec -i $POSTGRES_SERVER psql -U postgres
+echo "\l" | docker exec -i $POSTGRES_SERVER psql -U postgres
