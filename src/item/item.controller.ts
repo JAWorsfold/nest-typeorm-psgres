ef936c5fcf34d80efc5d2b02896ed69a8a8c99c1
joe.worsfold <joseph.worsfold@infinityworks.com>
2021-12-07 17:31:09 +0000

    item module dto and swagger doc

---
+++ src/item/item.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ItemService } from './item.service';
+import { ItemDTO } from './item.dto';
+import { User } from '../user.decorator';
+
+@Controller('item')
+export class ItemController {
+  constructor(private serv: ItemService) {}
+
+  @Get()
+  public async getAll(): Promise<ItemDTO[]> {
+    return await this.serv.getAll();
+  }
+
+  @Post()
+  public async post(
+    @User() user: User,
+    @Body() dto: ItemDTO,
+  ): Promise<ItemDTO> {
+    return this.serv.create(dto, user);
+  }
+}
